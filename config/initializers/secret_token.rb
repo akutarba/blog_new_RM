deba7d61c7e5825c650e806d40dffd1db531c1ec
akutarba <anna.kutarba@jetbrains.com>
2013-04-09 19:59:24 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogNewRm::Application.config.secret_key_base = '27dc3978cccfb800115cbfda3a16e06634780991e435230dac9d31a3a67ff969c775b26e530e532eebe9c0a4fdfe45cf6349c0c7421b05742faf49f073172b7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogNewRm::Application.config.session_store :encrypted_cookie_store, key: '_blog_new_RM_session'
